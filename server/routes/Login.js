aa49de4f1724b90b4f4562ff1ebf35d8f6f98174
Darshan <darshanrgs45@gmail.com>
2023-05-16 21:51:08 +0530

    Refactor APIs using controllers

--- server/routes/Login.js
+++ server/routes/Login.js
@@ -1,64 +1,65 @@
 const express = require('express')
 const router = require('express').Router()
-const bcrypt = require('bcrypt')
-const User = require("../model/user");
-const jwt = require("jsonwebtoken")
 
-const secret = "officeSolution"
+const {Login}=require("../controllers/Login")
+
 
 router.use(express.json())
 router.use(express.urlencoded())
 
-router.post('/login', async (req, res) => {
+router.route('/login').post(Login)
 
-    try {
-        const { email, password } = req.body;
-        const findQueryinDB = await User.findOne({ email: email });
-        console.log(req.body)
 
-        if (!findQueryinDB) {
-            return res.status(404).json({
-                status: "Error",
-                message: "User isn't register,please register before signin"
-            });
+// router.post('/login', async (req, res) => {
 
-        } else {
-            //  for passwordvallidation
-            console.log(req.body)
+//     try {
+//         const { email, password } = req.body;
+//         const findQueryinDB = await User.findOne({ email: email });
+//         console.log(req.body)
 
-            //privacy
-            bcrypt.compare(password, findQueryinDB.password, (err, result) => {
-                //authentication process by bcrypt
+//         if (!findQueryinDB) {
+//             return res.status(404).json({
+//                 status: "Error",
+//                 message: "User isn't register,please register before signin"
+//             });
 
-                console.log(result, "from bcrypt")
-                if (!result) {
-                    return res.status(403).json({
-                        status: "Failed",
-                        message: "Invalid User Password"
-                    })
-                }
-                else {
-                    //after successfull login we have to authorize the user by sstoring token to the user cookies/local storage
-                    const token = jwt.sign({
-                        // 60 hours from the current time.
-                        exp: Math.floor(Date.now() / 1000) + (60 * 60 * 60 * 60),
-                        findQueryinDB: findQueryinDB._id
-                    }, secret);
+//         } else {
+//             //  for passwordvallidation
+//             console.log(req.body)
 
-                    const userdetails = { ...findQueryinDB._doc, password: undefined }
-                    return res.status(200).json({
-                        status: "Success",
-                        message: { token, userdetails }
-                        //we are storing the token in the cookies
-                    })
-                }
-            })
+//             //privacy
+//             bcrypt.compare(password, findQueryinDB.password, (err, result) => {
+//                 //authentication process by bcrypt
 
-        }
+//                 console.log(result, "from bcrypt")
+//                 if (!result) {
+//                     return res.status(403).json({
+//                         status: "Failed",
+//                         message: "Invalid User Password"
+//                     })
+//                 }
+//                 else {
+//                     //after successfull login we have to authorize the user by sstoring token to the user cookies/local storage
+//                     const token = jwt.sign({
+//                         // 60 hours from the current time.
+//                         exp: Math.floor(Date.now() / 1000) + (60 * 60 * 60 * 60),
+//                         findQueryinDB: findQueryinDB._id
+//                     }, secret);
 
-    } catch (err) {
-        console.log(err)
-    }
-})
+//                     const userdetails = { ...findQueryinDB._doc, password: undefined }
+//                     return res.status(200).json({
+//                         status: "Success",
+//                         message: { token, userdetails }
+//                         //we are storing the token in the cookies
+//                     })
+//                 }
+//             })
 
-module.exports = router
+//         }
+
+//     } catch (err) {
+//         console.log(err)
+//     }
+// })
+
+module.exports=router
